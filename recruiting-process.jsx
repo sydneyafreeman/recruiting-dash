57f70a21666b239afb955a53f27c835625701cbf
sydneyafreeman <sydney@srfreemaninc.com>
2026-04-03 11:45:13 -0700

    Create recruiting-process.jsx

---
+++ recruiting-process.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const RecruitingProcess = () => {
+  return (
+    <div>
+      <h2>Recruiting Process</h2>
+      {/* Implement drag-and-drop functionality here */}
+      <div>
+        {/* Tiles for each candidate */}
+      </div>
+    </div>
+  );
+};
+
+export default RecruitingProcess;
